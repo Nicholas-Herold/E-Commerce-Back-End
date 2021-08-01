f41365ae9b8708d02bb22c9bbc27efb28c0bbe79
Nicholas Herold <nherold86@gmail.com>
2021-07-31 21:47:43 -0400

    auto pop seeds on startup and connect tables in model/ index

--- models/index.js
+++ models/index.js
@@ -5,13 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category,{})
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+
+  }
+);
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through:{
+    model:ProductTag
+  }
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through:{
+    model:Product
+  }
+})
 
 module.exports = {
   Product,
@@ -19,3 +35,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
